9393eac19cde01e681bad530b36713397a6b2796
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-18 22:32:41 -0500

    Adjusts component file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './components/Header/index.jsx';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Coffee</h1>
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="f1 fw2">Dennis Cortes</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
