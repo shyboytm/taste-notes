88ed5a9b12a63fece9c10cba733c253e7da3c785
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-19 14:53:12 -0500

    Adds About component and router additions

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { render } from 'react-dom';
 import './App.css';
 
 import Header from './components/Header/index.jsx';
+import About from './components/About/index.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App bg-light-gray">
+      <div className="App">
         <Header />
       </div>
     );
@@ -19,6 +20,7 @@ class App extends Component {
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+      <Route path="about" component={About} />
     </Route>
   </Router>
 ), document.getElementById('root'))
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import { Router, Route, browserHistory } from 'react-router';
-import { render } from 'react-dom';
 
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <div className="db dt-l w-100 border-box pa3 ph5-l">
+        <div className="bb b--black-10 db dt-l w-100 border-box pa3 ph5-l">
           <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2-l mb4 mb0-l" href="#" title="Home">
-            <img src="/img/logo.jpg" className="mb3 dib w2 h2 br-100" alt="coffee.cortes" />
-            <span className="cutive db v-mid">Coffee Thoughts &amp; Reference</span>
+            {/* <img src="/img/logo.jpg" className="mb3 dib w2 h2 br-100" alt="coffee.cortes" /> */}
+            <h1 className="mb1">C</h1>
+            <span className="cutive db">Coffee Thoughts &amp; Reference</span>
           </a>
           <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
             <a className="biorhyme link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Coffee">Coffee</a>
