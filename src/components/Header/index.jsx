c9e1bb8bd2d70970baaaa402f02095de35021054
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-20 15:45:38 -0500

    Changing logo to reflect name

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,7 @@ class Header extends Component {
       <div>
         <div className="bb b--black-10 db dt-l w-100 border-box pa3 ph5-l">
           <a className="db dtc-l v-mid mid-gray link hover-red w-100 w-25-l tc tl-l mb2-l mb4 mb0-l" href="#" title="Home">
-            <h2 className="biorhyme fw7 tracked-mega">CT&R</h2>
+            <h2 className="biorhyme f4 red hover-black fw7 ttu tracked-mega">Taste Notes</h2>
           </a>
           <div className="db dtc-l v-mid w-100 w-75-l tc tr-l mb0-l mb3">
             <a className="biorhyme link hover-red black-80 f5-l dib mr3 mr4-l" href="#" title="Coffee">Coffee</a>
