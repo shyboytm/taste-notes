1d285d554e1f76f32d03c791e6a54a0a98a7de32
David O'Trakoun <dotrakoun@elitedaily.com>
2016-10-20 14:37:06 -0400

    router is not rendered so link does not work

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
-import { render } from 'react-dom';
 
 import './App.css';
-
-import Home from './components/Home/index.jsx';
-import About from './components/About/index.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        {this.props.children}
       </div>
     );
   }
 }
-
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="about" component={About} />
-    </Route>
-  </Router>
-), document.getElementById('root'))
 
 export default App;
