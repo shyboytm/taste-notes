86790fc93c842aa395a98515d38de77d731c55a5
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-21 11:56:53 -0500

    Adds footer component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-
 class Header extends Component {
   render() {
     return (
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Header from '../Header/index.jsx';
 import Coffee from '../Coffee/index.jsx';
+import Footer from '../Footer/index.jsx';
 
 class Home extends Component {
   render() {
@@ -9,6 +10,7 @@ class Home extends Component {
       <div className="Home">
         <Header />
         <Coffee />
+        <Footer />
       </div>
     );
   }
