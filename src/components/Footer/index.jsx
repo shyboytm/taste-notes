917d00a462e089d3cdf7b3c8e1b2bca662235278
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-21 15:56:19 -0500

    Updates footer and build folder

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Footer extends Component {
   render() {
     return (
       <div className="pv4 w-90 center tl-l mb2-l mb4 mb0-l">
-        <a className="link tc" href="#" title="Home">
+        <Link className="link tc" to="/" title="Home">
           <h2 className="biorhyme f5 black-60 hover-black lh-copy fw7 ttu tracked-mega">Taste<br />Notes</h2>
-        </a>
+        </Link>
         <a className="mb2 db space-mono tc i black-30 link lh-copy hover-red" href="http://cortes.us">by Dennis Cortés</a>
         <span className="f6 db biorhyme tc black-30 link">© 2016</span>
       </div>
