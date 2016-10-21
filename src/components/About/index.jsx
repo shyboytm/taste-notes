a371d73c7172f7b1b6a0eed5a646fbd1229524fe
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-21 11:58:24 -0500

    Adds Footer to About component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from '../Footer/index.jsx';
 
 
 class About extends Component {
@@ -6,6 +7,7 @@ class About extends Component {
     return (
       <div>
         <h1>About</h1>
+        <Footer />
       </div>
     );
   }
