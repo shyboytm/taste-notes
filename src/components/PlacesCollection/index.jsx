3f4220f76dbd9393f1b20d7c202d4454970951bd
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-27 14:24:37 -0400

    Adds coffee shop info to Places Page

--- src/components/PlacesCollection/index.jsx
+++ src/components/PlacesCollection/index.jsx
@@ -5,34 +5,33 @@ import PlacesPost from '../PlacesPost/index.jsx'
 class PlacesCollection extends Component {
   render() {
 
-    const imgThree =  require('../../img/vista-hermosa.jpg');
-    const imgTwo =    require('../../img/las-colinas.jpg');
+    const imgTwo =    require('../../img/barista-parlor-gt.jpg');
     const imgOne =    require('../../img/hakimson-estate-peaberry.jpg');
 
     return (
       <div className="w-90-ns w-100 pv5-ns center cf">
         <div className="content-end flex-wrap">
           <PlacesPost
-            title="Vista Hermosa"
-            location="Sweet Bloom"
+            title="Barista Parlor Germantown"
+            location="Nashville, TN"
             thoughts="Red Plum, White Grape, Brown Sugar"
             pros="Guatemala"
             cons="Really bright and sweet taste overall with a great aftertaste.
                       Might be a bit too sweet for most coffee drinkers but I'm usually
                       a fan of sweeter and lighter roasts."
             rating="8.5 / 10"
-            shopImageSource={imgThree} />
+            shopImageSource={imgTwo} />
 
           <PlacesPost
-            title="Las Colinas"
-            location="George Howell Coffee"
+            title="Eighth & Roast"
+            location="Nashville, TN"
             thoughts="Passion Fruit, Chocolate, Black Tea"
             pros="Nicaragua"
             cons="Really bright and sweet taste overall with a great aftertaste.
                       Might be a bit too sweet for most coffee drinkers but I'm usually
                       a fan of sweeter and lighter roasts."
             rating="7.5 / 10"
-            shopImageSource={imgTwo} />
+            shopImageSource={imgOne} />
         </div>
       </div>
     );
