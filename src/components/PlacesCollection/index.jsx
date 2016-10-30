69e59420788f9a2e7e8a4061d4abdde90704cdb7
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-30 10:16:04 -0400

    Adds Eighth and Roast and changes layout of shops

--- src/components/PlacesCollection/index.jsx
+++ src/components/PlacesCollection/index.jsx
@@ -6,13 +6,23 @@ class PlacesCollection extends Component {
   render() {
 
     const imgFour =   require('../../img/barista-parlor-gt.jpg');
-    const imgThree =  require('../../img/comet-coffee.jpg');
-    // const imgTwo =    require('../../img/hakimson-estate-peaberry.jpg');
+    const imgThree =  require('../../img/eighth-and-roast.jpg');
+    const imgTwo =    require('../../img/comet-coffee.jpg');
     // const imgOne =    require('../../img/hakimson-estate-peaberry.jpg');
 
     return (
       <div className="w-90-ns w-100 pv5-ns center cf">
         <div className="content-end flex flex-wrap">
+          <PlacesPost
+            title="Comet Coffee"
+            location="Saint Louis, MO"
+            good="Pastries are amazing, great care for their coffee brewing, employees
+                  are very kind and helpful, outlets everywhere"
+            bad="Not much seating, very crowded if you come in at the wrong time, espresso drinks
+                 aren't very good"
+            rating="8.5 / 10"
+            shopImageSource={imgTwo} />
+
           <PlacesPost
             title="Barista Parlor GT"
             location="Nashville, TN"
@@ -22,35 +32,25 @@ class PlacesCollection extends Component {
                  hard to come by  to come by  to come by  to come by  to come by"
             rating="8.5 / 10"
             shopImageSource={imgFour} />
-
-          <PlacesPost
-            title="Comet Coffee"
-            location="Saint Louis, MO"
-            good="Pastries are amazing, great care for their coffee brewing, employees
-                  are very kind and helpful, outlets everywhere"
-            bad="Not much seating, very crowded if you come in at the wrong time, espresso drinks
-                 aren't very good"
-            rating="9 / 10"
-            shopImageSource={imgThree} />
         </div>
 
         <div className="content-end flex flex-wrap">
+          <PlacesPost
+            title="Eighth & Roast"
+            location="Nashville, TN"
+            good="Great espresso drinks, awesome pastries and breakfast foods, decent amount of
+                  natural light"
+            bad="Can get very crowded, black coffee isn't too great, seating uncomfortable
+                 for long periods of sitting"
+            rating="7 / 10"
+            shopImageSource={imgThree} />
+          
           <PlacesPost
             title="Barista Parlor Germantown"
             location="Nashville, TN"
             good="Guatemala"
             bad="Great ambiance, beautiful interior, usually quiet, incredible coffee and food"
             rating="8.5 / 10"
-            shopImageSource={imgThree} />
-
-          <PlacesPost
-            title="Comet Coffee"
-            location="Saint Louis, MO"
-            pros="Nicaragua"
-            cons="Really bright and sweet taste overall with a great aftertaste.
-                      Might be a bit too sweet for most coffee drinkers but I'm usually
-                      a fan of sweeter and lighter roasts."
-            rating="9 / 10"
             shopImageSource={imgThree} />
         </div>
       </div>
