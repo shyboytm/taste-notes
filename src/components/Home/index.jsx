8413149822fbc9f9f0cc40f176e5b76c914b3a4c
cortesarts <dennis.cortes@satchelhealth.com>
2016-11-05 16:38:25 -0700

    Adds title to coffee page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -9,6 +9,12 @@ class Home extends Component {
     return (
       <div className="Home">
         <Header />
+        <div className="w-90 pv4-ns center">
+          <h2 className="tc mv4 f1-ns f2 biorhyme fw3 red">Coffee</h2>
+          <p className="tc f4 space-mono lh-copy black-60">
+            Coffee roasts I have tried and my thoughts on them
+          </p>
+        </div>
         <CoffeeCollection />
         <Footer />
       </div>
