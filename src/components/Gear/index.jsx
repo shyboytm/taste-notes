9844df2d125a0bf8cbb9a628ef93d63fd1caed1a
cortesarts <dennis.cortes@satchelhealth.com>
2016-11-25 09:41:05 -0600

    Adds analytics and fixes text margins on mobile

--- src/components/Gear/index.jsx
+++ src/components/Gear/index.jsx
@@ -12,11 +12,11 @@ class Gear extends Component {
         <div className="w-90 pv4-ns center">
           <h2 className="tc mv4 f1-ns f2 biorhyme fw3 red">Gear</h2>
           <p className="tc f4 space-mono lh-copy black-60">
-            My favorite coffee gear that I use
+            My favorite coffee gear that I use, a worthy investment!
           </p>
         </div>
         <GearCollection />
-        <p className="tc f5 space-mono lh-copy black measure center">
+        <p className="w-40-l w-70-m w-90 tc f5 space-mono lh-copy black center">
           Any purchases you make here directly support me and this website as well as my other side projects. I appreciate your support!
         </p>
         <Footer />
